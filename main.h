72c0d8ae05304652f77365bf11b7dcbc1b503a48
TheElyphant <eodagba@outlook.com>
2023-09-18 07:52:32 +0100

    0-printf.c

--- main.h
+++ main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <limits.h>
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 #endif
