5c7e3cb03c0287444d9b0578b1cb3f0aec769994
TheElyphant <eodagba@outlook.com>
2023-09-18 09:23:56 +0100

    0-printf.c

--- 0-printf.c
+++ 0-printf.c
@@ -8,14 +8,14 @@ int _printf(const char *format, ...);
 int _printf(const char *format, ...)
 {
 	int print_c = 0;
-	va_list list_of_args;
+	va_list args;
 	char *str;
 	char c;
 	int str_len;
 
 	if (format == NULL)
 		return (-1);
-	va_start(list_of_args, format);
+	va_start(args, format);
 	while (*format)
 	{
 		if (*format != '%')
@@ -23,32 +23,39 @@ int _printf(const char *format, ...)
 			write(1, format, 1);
 			print_c++;
 		}
-		else if (*(++format) == '\0')
-			break;
-		if (*format == '%')
+		else
 		{
-			write(1, format, 1);
-			print_c++;
-		}
-		else if (*format == 'c')
-		{
-			c = va_arg(list_of_args, int);
-			write(1, &c, 1);
-			print_c++;
-		}
-		else if (*format == 's')
-		{
-			str = va_arg(list_of_args, char*);
-			str_len = 0;
-			while (str[str_len] != '\0')
+			format++;
+			if (*format == '\0')
+				break;
+			if (*format == '%')
 			{
-				str_len++;
-				write(1, str, str_len);
-				print_c += str_len;
+				write(1, "%", 1);
+				print_c++;
 			}
+			else if (*format == 'c')
+			{
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				print_c++;
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char*);
+				if (str != NULL)
+				{
+					str_len = 0;
+					while (str[str_len] != '\0')
+					{
+						str_len++;
+					}
+					write(1, str, str_len);
+					print_c += str_len;
+				}
+			}
+			format++;
 		}
-		format++;
-	}
-	va_end(list_of_args);
+	}	
+	va_end(args);
 	return (print_c);
 }
