0f676e4e1357ff3992c7f320b497f22d623812d2
TheElyphant <eodagba@outlook.com>
2023-09-19 22:57:53 +0100

    unsigned_formatters.c

--- unsigned_formatters.c
+++ unsigned_formatters.c
@@ -11,7 +11,7 @@
  * Return: The count of characters outputted.
  */
 int print_unsigned(va_list types, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
@@ -20,15 +20,12 @@ int print_unsigned(va_list types, char buffer[],
 
 	if (num == 0)
 		buffer[i--] = '0';
-
 	buffer[BUFF_SIZE - 1] = '\0';
-
 	while (num > 0)
 	{
 		buffer[i--] = (num % 10) + '0';
 		num /= 10;
 	}
-
 	i++;
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
 }
@@ -44,21 +41,18 @@ int print_unsigned(va_list types, char buffer[],
  * Return: Count of characters produced.
  */
 int print_octal(va_list types, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
 	UNUSED(width);
-
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
 		buffer[i--] = '0';
-
 	buffer[BUFF_SIZE - 1] = '\0';
-
 	while (num > 0)
 	{
 		buffer[i--] = (num % 8) + '0';
@@ -81,10 +75,10 @@ int print_octal(va_list types, char buffer[],
  * Return: Count of characters produced.
  */
 int print_hexadecimal(va_list types, char buffer[],
-		int flags, int width, int precision, int size)
+	int flags, int width, int precision, int size)
 {
 	return (print_hexa(types, "0123456789abcdef", buffer,
-				flags, 'x', width, precision, size));
+	flags, 'x', width, precision, size));
 }
 
 /**
@@ -98,10 +92,10 @@ int print_hexadecimal(va_list types, char buffer[],
  * Return: Count of characters produced.
  */
 int print_hexa_upper(va_list types, char buffer[],
-		int flags, int width, int precision, int size)
-{'
-	return (print_hexa(types, "0123456789ABCDEF", buffer,
-				flags, 'X', width, precision, size));
+	int flags, int width, int precision, int size)
+{
+	return (print_hexa(types, "0123456789ABCDEF",
+	buffer, flags, 'X', width, precision, size));
 }
 
 /**
@@ -117,21 +111,18 @@ int print_hexa_upper(va_list types, char buffer[],
 * Return: Count of characters produced.
 */
 int print_hexa(va_list types, char map_to[], char buffer[],
-		int flags, char flag_ch, int width, int precision, int size)
+	int flags, char flag_ch, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
 	UNUSED(width);
-
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
 		buffer[i--] = '0';
-
 	buffer[BUFF_SIZE - 1] = '\0';
-
 	while (num > 0)
 	{
 		buffer[i--] = map_to[num % 16];
