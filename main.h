097c7c9ded22eea0d6bdcb185fab9b395dc58a9a
salsdev <salehaliyu111@gmail.com>
2023-09-19 19:41:58 +0000

    fix

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-#include <string.h>
+#include <stdio.h>
 #include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
 
 int _putchar(char c);
 int _printf(const char *format, ...);
